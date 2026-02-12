f6109ec3073a63d4b46a3a4ae92bd420c76aa8ca
Shan277 <shanjod96@gmail.com>
2026-02-13 02:50:26 +0530

    Main Branch

---
+++ example.py
@@ -0,0 +1,79 @@
+#      ANKIT
+print("We are starting our new project")
+enter =input("Enter your project name: ")
+enter1=input("Enter the number of members: ")
+enter2=input("Key idea of your project: ")
+
+print("hello")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#       AYAN
+
+
+print("bal")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#         BIBHU
+print("Hello World!")
+name = input("Enter your name: ")
+print(f"{name}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#           SHREYAS
+
+print("Hello World")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
