de5df72a66cdb59a45b5f9562e5c123c2dcd457e
Shan277 <shanjod96@gmail.com>
2026-02-14 21:49:00 +0530

    Database Connection

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,18 @@
 from flask import Flask, request
 from flask_cors import CORS
+import mysql.connector
 
 app = Flask(__name__)
 CORS(app)
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="799277",
+    database="register_db"
+)
+
+
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -17,7 +27,13 @@ def register():
     password = data["password"]
 
     print(username, password)
-
+    
+    cursor = db.cursor()
+    
+    cursor.execute("insert into users(name,password) values(%s,%s)", (username,password))
+    db.commit()
+    cursor.close()
+    
     return "Registered Successfully"
 
 if __name__ == "__main__":
